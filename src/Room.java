fb06178017e665cbd5e65eb6673edf7482b6e6dd
georgetse5 <geo123123123@gmail.com>
2024-04-13 10:26:55 +0300

    Room superclass added

---
+++ src/Room.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Room {
+    String name;
+    String description;
+    Map<String, Room> exits;
+
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+        exits = new HashMap<>();
+    }
+
+
+    public void addExit(String direction, Room destination) {
+        exits.put(direction, destination);
+    }
+
+
+    public Room getExit(String direction) {
+        return exits.get(direction);
+    }
+}
+
