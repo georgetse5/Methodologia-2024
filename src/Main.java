f00f5541bec59fc01a7c92437f8b884223949af6
georgetse5 <74043313+georgetse5@users.noreply.github.com>
2024-05-26 09:58:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -21,4 +21,4 @@ public class Main {
 
 // TODO: 20/5/2024:
 //  1) Add more items and containers
-//  2) Make more quests which the player can make a progress and eventually he wins or loses the game.
+//  2) Make more quests which the player can make or lose progress and eventually he wins or loses the game.
